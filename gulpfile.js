bd3b89a5cd29e385d7534a03de7f33bc3138a6d4
Sen Li <ls.learnshare@gmail.com>
2016-02-16 10:24:21 -0500

    init

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp'),
+    webserver = require('gulp-webserver');
+
+gulp.task('webserver', function () {
+    gulp.src('app')
+        .pipe(webserver({
+            livereload: true,
+            open: true,
+            port: 8180,
+        }));
+});
